c6319c69c524ad3d86570c322f239b5faa5bc00c
Guarin0829 <daniela.lopezg1@uqvirtual.edu.co>
2024-03-18 13:14:10 -0500

    Se agrega punto 11 del taller

--- src/main/java/org/example/laboratorio/ejercicio11/MainEjercicio11.java
+++ src/main/java/org/example/laboratorio/ejercicio11/MainEjercicio11.java
@@ -1,4 +1,20 @@
 package org.example.laboratorio.ejercicio11;
 
 public class MainEjercicio11 {
+
+    public static void main(String[] args) {
+
+        ListaEnlazada polinomio = new ListaEnlazada();
+        polinomio.agregarTermino(1, 2);
+        polinomio.agregarTermino(9, 1);
+        polinomio.agregarTermino(11, 0);
+
+
+        System.out.println("Tabla de valores del polinomio:");
+        System.out.println("x\tResultado");
+        for (double x = 0.0; x <= 5.0; x += 0.5) {
+            double resultado = polinomio.evaluar(x);
+            System.out.println(x + "\t" + resultado);
+        }
+    }
 }
