3b995e98c67b866f99a681d6ef9fcd9c72e1c518
José Yovany Beltrán Matallana <101234042+JYBM78@users.noreply.github.com>
2024-03-19 14:43:21 -0500

    Update MainEjercicio7.java
    Se agrega la clase main

--- src/main/java/org/example/laboratorio/ejercicio7/MainEjercicio7.java
+++ src/main/java/org/example/laboratorio/ejercicio7/MainEjercicio7.java
@@ -1,4 +1,24 @@
 package org.example.laboratorio.ejercicio7;
 
-public class MainEjercicio7 {
+/*
+ *Ejercicio 7. Escribir el Iterador para una lista doblemente enlazada.
+ */
+
+// Clase Main para probar el iterador
+class Main {
+    public static void main(String[] args) {
+        ListaDoblementeEnlazada lista = new ListaDoblementeEnlazada();
+        lista.insertarAlFinal(1);
+        lista.insertarAlFinal(2);
+        lista.insertarAlFinal(3);
+        lista.insertarAlFinal(4);
+        lista.insertarAlFinal(5);
+
+        // Iterar la lista e imprimir los elementos
+        System.out.println("Iterando la lista:");
+        for (int elemento : lista) {
+            System.out.print(elemento + " ");
+        }
+        System.out.println();
+    }
 }
